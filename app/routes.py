872471e6371b4cfffa413d5d754408a4a19676d3
thommandrugurusaijyotheeswar <thommandrugurusaijyotheeswar@gmail.com>
2026-07-12 10:19:33 +0530

    Added dashboard UI with sidebar

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
-def home():
+def login():
     return render_template("index.html")
+
+@main.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@main.route("/login")
+def login_redirect():
+    return redirect("/dashboard")
